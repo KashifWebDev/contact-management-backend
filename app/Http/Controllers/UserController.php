512467b273d230e2377d27229476b35e66925f39
Kashif <admin@kashifali.me>
2024-02-07 12:27:31 +0500

    Routes & Controller Added

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        $users = User::all();
+        return response()->json($users);
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email'
+        ]);
+
+        $user = User::create($request->all());
+        return response()->json($user, 201);
+    }
+
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+        return response()->json(null, 204);
+    }
+}
